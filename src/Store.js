31f13726523fdacfcf11b046d09adc451a1f2a7c
1447bits <untilinfinity00@gmail.com>
2023-09-23 12:45:46 +0530

    create empty login pg components

--- src/Store.js
+++ src/Store.js
@@ -2,11 +2,25 @@ import { proxy } from "valtio";
 
 const store = proxy({
     // all states components
-    sampleState: "Type in above input"
+    sampleState: "Type in above input",
+
+    xp:300,
+
+    // 1 english
+    // 2 hindi
+    currLanguage: 1,
+
+    mascotDialog: "Hey there",
 })
 
 export function updatesamplestate( val ) {
     store.sampleState = val
 }
+
+export function updateXp( val ) {
+    store.xp = val
+}
+
+
 
 export default store;
